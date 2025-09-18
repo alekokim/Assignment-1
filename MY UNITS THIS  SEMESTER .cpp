275d9861b6e1be4d9a1fc0df0152cf6cb404139a
alekokim <alekokim95@gmail.com>
2025-09-18 12:12:56 +0300

    Add files via upload

---
+++ MY UNITS THIS  SEMESTER .cpp
@@ -0,0 +1,29 @@
+#// simple C program(my units)single line comment
+/*
+Name:Alex
+Reg:CT100/G/26186/25
+Description:My units this semester 
+*/
+#include <stdio.h>
+
+int main() {
+    printf("=====================================\n");
+    printf("  My units this semester \n");
+    printf("=====================================\n\n");
+
+    printf("1. Introduction to Computer Application\n");
+    printf("2. Introduction to Programming and Algorithms\n");
+    printf("3. Mathematics for science\n");
+    printf("4. Communication Skills\n");
+    printf("5. Fundamentals of Information Technology\n");
+    printf("6. Computer system and Organisation \n");
+    printf("7. Analogue Electronics \n");
+    printf("8.Essential of Accounting \n");
+    
+    printf("\n=====================================\n");
+    printf("   End of Units List\n");
+    printf("=====================================\n");
+
+    return 0;
+}
+    
